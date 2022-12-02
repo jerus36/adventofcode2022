f8cc6c80814fe317d490c39c7f31fc935af54aa4
Jeremy King <jking@oilygears.com>
2022-12-02 17:16:19 -0500

    Day 2

--- days/day2/day2.go
+++ days/day2/day2.go
@@ -3,31 +3,37 @@ package day2
 import (
 	"adventofcode2022/internal/util"
 	"fmt"
-	"sort"
-)
-
-const (
-	Rock     int = 1
-	Paper        = 2
-	Scissors     = 3
-	Win          = 0
-	Lose         = 1
-)
-const (
-	A string = "Rock"
-	B        = "Paper"
-	C        = "Scissors"
 )
 
 // func getScore(opp string, you string) int {
 
 // }
+const (
+	Rock     int = 1
+	Paper        = 2
+	Scissors     = 3
+	Win          = 6
+	Lose         = 0
+	Draw         = 3
+)
 
 func Solve() {
-	lines := util.ReadLines("2")
-	sort.Strings(lines)
 
-	for _, line := range lines {
-		fmt.Println(line)
+	lines := util.ReadLines("2")
+	m := map[string]int{
+		"A X": Rock + Draw,
+		"A Y": Paper + Win,
+		"A Z": Scissors + Lose,
+		"B X": Rock + Lose,
+		"B Y": Paper + Draw,
+		"B Z": Scissors + Win,
+		"C X": Rock + Win,
+		"C Y": Paper + Lose,
+		"C Z": Scissors + Draw,
 	}
+	val := 0
+	for _, line := range lines {
+		val += m[line]
+	}
+	fmt.Println("Score:", val)
 }
