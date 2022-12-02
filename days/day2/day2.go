d371e611ebcb35f6d9dff34225d5c9d6949077e7
Jeremy King <jking@oilygears.com>
2022-12-02 17:48:13 -0500

    Day2 part2

--- days/day2/day2.go
+++ days/day2/day2.go
@@ -17,9 +17,22 @@ const (
 	Draw         = 3
 )
 
-func Solve() {
+func part2_map() map[string]int {
+	m := map[string]int{
+		"A X": Scissors + Lose,
+		"A Y": Rock + Draw,
+		"A Z": Paper + Win,
+		"B X": Rock + Lose,
+		"B Y": Paper + Draw,
+		"B Z": Scissors + Win,
+		"C X": Paper + Lose,
+		"C Y": Scissors + Draw,
+		"C Z": Rock + Win,
+	}
+	return m
+}
 
-	lines := util.ReadLines("2")
+func part1_map() map[string]int {
 	m := map[string]int{
 		"A X": Rock + Draw,
 		"A Y": Paper + Win,
@@ -31,9 +44,23 @@ func Solve() {
 		"C Y": Paper + Lose,
 		"C Z": Scissors + Draw,
 	}
+	return m
+}
+
+func Solve() {
+	lines := util.ReadLines("2")
+	m := part1_map()
 	val := 0
 	for _, line := range lines {
 		val += m[line]
 	}
-	fmt.Println("Score:", val)
+	fmt.Println("Part1:", val)
+
+	m = part2_map()
+	val = 0
+	for _, line := range lines {
+		val += m[line]
+	}
+	fmt.Println("Part2:", val)
+
 }
